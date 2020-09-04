5d2aa2f0b7963309d88e57420b6696936931f12e
xdebbie <deh@live.in>
2020-09-04 22:59:26 +0200

    Create pagination for archive on node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,6 @@
 /**
  * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-// You can delete this file if you're not using it
 
 const path = require(`path`)
 const { resolve } = require('path')
@@ -22,11 +18,11 @@ const makeRequest = (graphql, request) =>
         )
     })
 
-// here we implement gatsby's API "createPages". this is called once the data layer is bootstrapped to let plugins create pages from data
+// Here we implement gatsby's API "createPages". this is called once the data layer is bootstrapped to let plugins create pages from data
 exports.createPages = ({ actions, graphql }) => {
     const { createPage } = actions
 
-    // now we create the pages for each article
+    // Creating the pages for each article { pagination }
     const getBlog = makeRequest(
         graphql,
         `
@@ -59,5 +55,52 @@ exports.createPages = ({ actions, graphql }) => {
         })
     })
 
-    return Promise.all([getBlog])
+    // Create archive page for all articles, including pagination
+    const getArchive = makeRequest(
+        graphql,
+        `
+    {
+        allContentfulBlog (
+            sort: { fields: createdAt, order: DESC }
+            filter: {
+                node_locale: { eq: "en-US" }},)
+        {
+            edges {
+                node {
+                    id
+                    slug
+                }
+            }
+        }
+    }
+    `
+    ).then(result => {
+        const blogs = result.data.allContentfulBlog.edges
+        const blogsPerPage = 12
+        const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+        // Array is the number of pages, and for each value "_" that it contains, it will create a page "i"
+        Array.from({ length: numPages }).forEach((_, i) => {
+            createPage({
+                /* This IF will analyse whether it is the first page (i === 0), if it is,
+                 * then it will create the url path "archive". If it's not, it will find its position
+                 * within the array (from the total number of pages, for example page 2, 3, etc).
+                 * The skip parameter is to requery the GraphQL every time we have a new page, so for example,
+                 * if the page is the second, then it will multiply i (1) times blogsPerPage (12), so it will
+                 * skip the first 12 items and start querying from the 13th article
+                 */
+
+                path: i === 0 ? `/archive` : `/archive/${i + 1}`,
+                component: path.resolve('./src/templates/archive.js'),
+                context: {
+                    limit: blogsPerPage,
+                    skip: i * blogsPerPage,
+                    numPages,
+                    currentPage: i + 1
+                }
+            })
+        })
+    })
+
+    return Promise.all([getBlog, getArchive])
 }
